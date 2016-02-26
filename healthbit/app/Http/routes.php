151fec29dee7417b5960ff7bf44e032a5564686b
Derrick Rono <derrickrono@gmail.com>
2016-02-26 15:29:52 +0300

    individual routes for specific content

--- healthbit/app/Http/routes.php
+++ healthbit/app/Http/routes.php
@@ -33,6 +33,12 @@ Route::group(['middleware' => ['web']], function () {
 Route::group(['prefix'=>'api'],function(){
 
 	Route::get('patientdata/{id}','PatientDataController@show');
+
+	Route::get('patientdata/prescriptions/{id}','PatientDataController@prescriptions');
+	Route::get('patientdata/labtest/{id}','PatientDataController@labtest');
+	Route::get('patientdata/imaging/{id}','PatientDataController@imaging');
+	Route::get('patientdata/diagnoses/{id}','PatientDataController@diagnoses');
+	Route::get('patientdata/metadata/{id}','PatientDataController@metadata');
 	Route::post('store_patient_data','PatientDataController@store');
 
 	/**
