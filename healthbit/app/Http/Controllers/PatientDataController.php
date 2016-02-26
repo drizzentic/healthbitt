72b6c5d0ccaf6029cd9866eebe60008eb64dd6b0
Derrick Rono <derrickrono@gmail.com>
2016-02-26 13:02:15 +0300

    special commit

--- healthbit/app/Http/Controllers/PatientDataController.php
+++ healthbit/app/Http/Controllers/PatientDataController.php
@@ -130,7 +130,7 @@ class PatientDataController extends Controller
 
         $items=json_decode(strstr($results, '{'));
 
-        return $items['patient_history'];
+        return $items->patient_history;
     }
 
     /**
