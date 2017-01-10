08b5d605d89db6e81afe0661e4ee02471b39cd3d
Derrick Rono <derrickrono@gmail.com>
2017-01-10 16:37:11 +0300

    modified files to allow posting, signing and broadcasting of raw transactions to the bitcoin testnet

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 var bitcoin = require('bitcoinjs-lib');
 var request = require('request');
 var http = require('http');
+var express = require('express');
+var app = express();
+var organization_id="";
 
-http.createServer(function (req, res) {
+app.post('/healthbit',function(req,res){
+var organization_id=req.param('organization_id');
+var data_reference=req.param('data_reference');
+var asset = {
+    //Put your testnet address
+    'issueAddress': '',
+    'amount': 1,
+    'divisibility': 0,
+    'fee': 10000,
+    'reissueable':false,
+    'transfer': [{
+        //Put your testnet destination address
+    	'address': '',
+    	'amount': 1
+    }],
+    'metadata': {
+        'assetName': organization_id,
+        'issuer': organization_id,
+        'description':data_reference 
+    }
+};
 
-    switch(req.url) { 
-    	 case '/healthbit':
-    	 postToApi('issue', asset, function(err, body){
+postToApi('issue', asset, function(err, body){
 		    if (err) {
 		        console.log('error: ', err);
 		    }
 
-		    //Sign Transaction
-			var key="KzpWk9XvAYWVERbkQzxHKKLmy1escmfjAr1PxkZLbG1UJVtdcFdt";
+		    //Sign Transaction with key generated using address.js
+			var key="";
 			var txHex=body.txHex;
 
 		    var signedTxHex = signTx(txHex, key);
@@ -24,26 +45,19 @@ http.createServer(function (req, res) {
 			    if (err) {
 			        console.log('error: ', err);
 			    }
-
+			
 			});
 
-			console.log("signedTxHex: ["+signedTxHex+"]");
-			res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({ a: body }));
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ response: body }));
 		});
-     
-    	 break;
-    	case 'default':
-    	 res.setHeader('Content-Type', 'application/json');
-         res.end(JSON.stringify("[404] " + req.method + " to " + req.url));
-    	
-    };
-}).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
+});
 
-key = bitcoin.ECKey.makeRandom();
-new_address = key.pub.getAddress(bitcoin.networks.testnet).toString();
+app.listen(1337, function () {
+  console.log('app listening on port 1337!');
+});
+
 function postToApi(api_endpoint, json_data, callback) {
     console.log(api_endpoint+': ', JSON.stringify(json_data));
     request.post({
@@ -58,31 +72,11 @@ function postToApi(api_endpoint, json_data, callback) {
         if (typeof body === 'string') {
             body = JSON.parse(body)
         }
-        console.log('Status: ', response.statusCode);
+        console.log('Status for : ', response.statusCode);
         console.log('Body: ', JSON.stringify(body));
         return callback(null, body);
     });
 };
-
-var asset = {
-    'issueAddress': 'mzhXGXuKvW4j34yJNszVRwkhd7XSFiG37h',
-    'amount': 100,
-    'divisibility': 0,
-    'fee': 1000,
-    'reissueable':false,
-    'transfer': [{
-    	'address': 'mzhXGXuKvW4j34yJNszVRwkhd7XSFiG37h',
-    	'amount': 100
-    }],
-    'metadata': {
-        'assetId': '1',
-        'assetName': 'Drizzentic Test',
-        'issuer': 'Asset Issuer',
-        'description': 'My Description'
-    }
-};
-
-
 
 
 function signTx (unsignedTx, wif) {
